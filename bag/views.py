e5ca68d6fb012c4eb936facc8f9e99e13a5963c1
Simon Boylan <siboy73@gmail.com>
2024-04-26 17:46:40 +0100

    Add notificationcss, bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404)
 from django.contrib import messages
 from products.models import Product
 
@@ -12,7 +13,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -24,13 +25,21 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(
+                    request, f'Updated quantity of {product.name} in size {size.upper()} to {bag[item_id]["items_by_size"][size]}') # noqa
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(
+                    request, f'Added {product.name} in size {size.upper()} to your bag!')  # noqa
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(
+                request, f'Added {product.name} in size {size.upper()} to your bag!')  # noqa
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(
+                request, f'Updated quantity of{product.name} in your {bag[item_id]}!')  # noqa
         else:
             bag[item_id] = quantity
             messages.success(
@@ -43,6 +52,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the shopping bag"""
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -52,15 +62,23 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(
+                request, f'Updated quantity of {product.name} in size {size.upper()} to {bag[item_id]["items_by_size"][size]}') # noqa
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(
+                    request, f'Removed {product.name} in size {size.upper()} from your bag!')  # noqa
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(
+                request, f'Updated quantity of{product.name} in your {bag[item_id]}!')  # noqa
         else:
             bag.pop(item_id)
+            messages.success(
+                request, f'Removed {product.name} in size {size.upper()} from your bag!')  # noqa
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -69,6 +87,7 @@ def adjust_bag(request, item_id):
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -78,11 +97,16 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(
+                    request, f'Removed {product.name} in size {size.upper()} from your bag!')  # noqa
         else:
             bag.pop(item_id)
+            messages.success(
+                request, f'Removed {product.name} in size {size.upper()} from your bag!')  # noqa
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
