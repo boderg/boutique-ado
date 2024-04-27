f2419e8a989d2080da62659350ed511678202708
Simon Boylan <siboy73@gmail.com>
2024-04-27 10:02:51 +0100

    Update environment variables

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 import os
 
+if os.path.exists('env.py'):
+    import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4g-b*$f2u%linf6xc_9b*k65f%o#%=(vayp*s!iz!p=gl(@59q'  # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = []
 
